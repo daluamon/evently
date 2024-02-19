1c62f3922dd2b672501a043e83795e770f0fa4db
daluamon <marcio.o.alves@hotmail.com>
2024-02-19 17:42:35 -0300

    Add clerk authentication and add header

---
+++ components/shared/MobileNav.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Separator } from "@/components/ui/separator"
+import NavItems from "./NavItems";
+
+
+const MobileNav = () => {
+  return (
+    <nav className="md:hidden">
+      <Sheet>
+        <SheetTrigger className="align-middle">
+          <Image
+            src="/assets/icons/menu.svg"
+            alt="Menu"
+            width={24}
+            height={24}
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+
+        <SheetContent className="bg-white flex flex-col gap-6 md:hidden">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="Brasilis Eventos"
+            height={38}
+            width={128}
+          />
+          <Separator className="border border-gray-50"/>
+          <NavItems />
+        </SheetContent>
+      </Sheet>
+    </nav>
+  );
+};
+
+export default MobileNav;
