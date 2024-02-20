25560da4f61ac3a312ccff2a15bd536963893e7b
daluamon <marcio.o.alves@hotmail.com>
2024-02-20 19:15:45 -0300

    feature: add form events

---
+++ components/shared/FileUploader.tsx
@@ -0,0 +1,21 @@
+import { Dispatch, SetStateAction } from "react";
+
+type FileUploadProps = {
+  imageUrl: string;
+  onFieldChange: (value: string) => void;
+  setFiles: Dispatch<SetStateAction<File[]>>;
+}
+
+const FileUploader = ({
+  imageUrl,
+  onFieldChange,
+  setFiles
+}: FileUploadProps) => {
+  return (
+    <div>
+      FileUploader
+    </div>
+  )
+}
+
+export default FileUploader;
